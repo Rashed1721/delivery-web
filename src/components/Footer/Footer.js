80d2cac23ea29eb2c4e9f3b62cb4d7554722beda
Rashed1721 <rashednsu172@gmail.com>
2022-02-05 11:14:55 +0600

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div style={{ marginTop: 50 }}>
+
+
+            <p style={{ margin: 0 }}>About Us| Contact Us| Careers |FAQ</p><br />
+            <p > Terms of Use | Messaging Terms | Privacy policy</p>
+            <h6 style={{ fontsize: 5 }}>	&copy; COPYRIGHT 2022| ALL RIGHT RESERVED</h6>
+        </div >
+    );
+};
+
+export default Footer;
---
+++ src/components/Home/NavBar/NavBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavBar.css'
+
+import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+const NavBar = () => {
+
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container style={{ margin: 'auto' }}>
+                    {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+
+                            <i style={{ color: '#0cce87', marginTop: 15 }} class="fas fa-map-marker-alt"></i>
+                            <NavDropdown title="Paris" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                            <Form className="d-flex">
+                                <i style={{ color: '#0cce87', marginTop: 15 }} class="fas fa-search"></i>
+                                <FormControl
+                                    style={{ borderColor: 'transparent' }}
+                                    type="search"
+                                    placeholder="your company"
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+
+                            </Form>
+                            <Nav.Link href="#home"><button className='signIn'>Sign in</button></Nav.Link>
+                            <Nav.Link href="#link"><button className='signUp'> Sign Up <i class="fas fa-long-arrow-alt-right"></i></button></Nav.Link>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default NavBar;
